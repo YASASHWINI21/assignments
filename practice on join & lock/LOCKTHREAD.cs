d20d4c730e3511b3780fa329f693abd4b6aa6f3a
YASASHWINI <72144826+YASASHWINI21@users.noreply.github.com>
2020-10-10 21:55:59 +0530

    Add files via upload

---
+++ practice on join & lock/LOCKTHREAD.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+class Printer
+{
+    public void PrintTable()
+    {
+        lock (this)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Printer p = new Printer();
+        Thread t1 = new Thread(new ThreadStart(p.PrintTable));
+        Thread t2 = new Thread(new ThreadStart(p.PrintTable));
+        t1.Start();
+        t2.Start();
+    }
+}
---
+++ practice on join & lock/jointhreading.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+public class ExThread
+{
+
+    // Non-Static method 
+    public void Mythread()
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            Console.WriteLine(x);
+            Thread.Sleep(100);
+        }
+    }
+
+    // Non-Static method 
+    public void Mythread1()
+    {
+        Console.WriteLine("2nd thread is Working..");
+    }
+}
+
+// Driver Class 
+public class ThreadExample
+{
+    // Main method 
+    public static void Main()
+    {
+        // Creating instance for 
+        // mythread() method 
+        ExThread obj = new ExThread();
+
+        // Creating and initializing threads 
+        Thread thr1 = new Thread(new ThreadStart(obj.Mythread));
+        Thread thr2 = new Thread(new ThreadStart(obj.Mythread1));
+        thr1.Start();
+
+        // Join thread 
+        thr1.Join();
+        thr2.Start();
+
+    }
+}
