a71c70f07525be7b9f7c4b17c2bfd18264b75fe8
YASASHWINI <72144826+YASASHWINI21@users.noreply.github.com>
2020-10-10 13:31:41 +0530

    Add files via upload

---
+++ lock n join ex/joinex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Demo
+{
+    public static void Main(string[] args)
+    {
+        string[] strArr = { "One", "Two", "Three", "Four" };
+        Console.WriteLine("String Array...");
+        foreach (string s in strArr)
+        {
+            Console.WriteLine(s);
+        }
+        string str = string.Join("/", strArr);
+        Console.WriteLine("Result (after joining) = " + str);
+    }
+}
---
+++ lock n join ex/lockex.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lockex
+{
+    class Program
+    {
+        static object x = new object();
+        static void AnotherMethod()
+        {
+            lock (x) { Console.WriteLine("another method "); }
+        }
+        static void Main(string[] args)
+        {
+            lock (x)
+            {
+                AnotherMethod();
+                //we still have the lock
+            }
+            Console.ReadLine();
+        }
+    }
+}
