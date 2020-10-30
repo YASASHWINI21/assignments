bde06541c7add6e1d27155b28b97c9de53c4561a
YASASHWINI <72144826+YASASHWINI21@users.noreply.github.com>
2020-10-30 08:16:45 +0530

    Add files via upload

---
+++ 28102020/datagrid.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DataGridEx.aspx.cs" Inherits="Aspgrid.DataGridEx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+			<asp:DataGrid ID="DataGrid1" runat="server"></asp:DataGrid>
+        </div>
+		<br /><br />
+		 <div>
+			<asp:DataGrid ID="DataGrid2" runat="server"></asp:DataGrid>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 28102020/railway.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Aspgrid
+{
+    public partial class Railway : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            using (SqlConnection Connect = new SqlConnection("data source=IN-PG00N524;database= priscilla1; Integrated Security=true"))
+            {
+                SqlDataAdapter SDA = new SqlDataAdapter("Select * from Railway", Connect);
+                DataSet ds = new DataSet();
+
+                SDA.Fill(ds);
+                DataGrid1.DataSource = ds;
+                DataGrid1.DataBind();
+
+
+            }
+
+
+        }
+
+        protected void DataGrid1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
