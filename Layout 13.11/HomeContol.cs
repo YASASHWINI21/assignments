5d7723d87fa4ea0718db864dc90ad2604bfe357e
YASASHWINI <72144826+YASASHWINI21@users.noreply.github.com>
2020-11-16 19:47:14 +0530

    Add files via upload

---
+++ Layout 13.11/HomeContol.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LayoutDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Home()
+        {
+            return View();
+        }
+		public ActionResult About()
+		{
+			return View();
+
+		}
+		public ActionResult Contact()
+		{
+			return View();
+		}
+	}
+}
