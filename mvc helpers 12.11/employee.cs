e26a55e9b48fd531d70f20c18776092d96bca5ff
YASASHWINI <72144826+YASASHWINI21@users.noreply.github.com>
2020-11-16 19:09:58 +0530

    Add files via upload

---
+++ mvc helpers 12.11/employee.cs
@@ -0,0 +1,32 @@
+﻿namespace MVC.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public bool IsHobbies { get; set; }
+        public Courses StudentCourses { get; set; }
+        public Skills SudentSkills { get; set; }
+
+    }
+
+    public enum Courses
+    {
+        Csharp,
+        DOTNetprogramming,
+        Html,
+        CSS,
+        ADODOTNETprogramming,
+        ASPDOTNETprogramming
+
+    }
+    public enum Skills
+    {
+        PythonPrograamming,
+        JavaProgramming,
+        CProgramming,
+        DBMS
+    }
+}
