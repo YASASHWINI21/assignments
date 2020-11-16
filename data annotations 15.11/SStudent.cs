d3e79a8bb066a9ac62b96dc627b4541ed4e4262b
YASASHWINI <72144826+YASASHWINI21@users.noreply.github.com>
2020-11-16 20:06:04 +0530

    Add files via upload

---
+++ data annotations 15.11/SStudent.cs
@@ -0,0 +1,20 @@
+@{
+    Layout = null;
+}
+
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title>Index</title>
+</head>
+<body>
+    <div>
+        @ViewData["Name"];
+        @ViewData["Check"];
+        @ViewData["As"];
+        @ViewData["LastName"];
+    </div>
+</body>
+</html>
